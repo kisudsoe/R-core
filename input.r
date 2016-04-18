f0c88f07130f0ca5016496599d6875a1c6835f9c
kisudsoe <kisudsoe@gmail.com>
2016-04-18 10:32:36 +0900

    Function input v2.0a
    ## 1. Data input ##
    # First written 2013
    # Last update 2014-10-09 THR
    # Version info 2.0a

---
+++ input.r
@@ -0,0 +1,13 @@
+## 1. Data input ##
+# First written 2013
+# Last update 2014-10-09 THR
+# Version info 2.0a
+
+input = function(i) {
+  if(i == 0) {
+  } else if(i == 1) {
+  } else if(i == 2) {
+  } else if(i == 3) {
+  }
+  return(data)
+}
