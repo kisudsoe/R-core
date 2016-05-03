0a4a744f3c97152ded105630aa36115fbe6c5e6d
kisudsoe <kisudsoe@gmail.com>
2016-05-03 10:22:56 +0900

    kmeans 관련 함수

---
+++ clustering/ss.heatmap.r
@@ -0,0 +1,47 @@
+﻿## Daw a Heat Map ##
+
+# heatmap(x, Rowv = NULL, Colv = if(symm)"Rowv" else NULL,
+#         distfun = dist, hclustfun = hclust,
+#         reorderfun = function(d, w) reorder(d, w),
+#         add.expr, symm = FALSE, revC = identical(Colv, "Rowv"),
+#         scale = c("row", "column", "none"), na.rm = TRUE,
+#         margins = c(5, 5), ColSideColors, RowSideColors,
+#         cexRow = 0.2 + 1/log10(nr), cexCol = 0.2 + 1/log10(nc),
+#         labRow = NULL, labCol = NULL, main = NULL,
+#         xlab = NULL, ylab = NULL,
+#         keep.dendro = FALSE, verbose = getOption("verbose"), ...)
+
+# rainbow(n, s = 1, v = 1, start = 0, end = max(1, n - 1)/n, alpha = 1)
+# heat.colors(n, alpha = 1)
+# terrain.colors(n, alpha = 1)
+# topo.colors(n, alpha = 1)
+# cm.colors(n, alpha = 1)
+
+ssheatmap = function(data) {
+	library("gplots")
+ 
+	x = as.matrix(data)
+	color.group = c(rep("#FF0000",3),rep("#FFFF00",3),rep("#FF00FF",3),rep("#00FFFF",3),rep("#888800",3))
+ 
+	#png(filename="heatmap.png", width=400, height=1080) # 실행전에 작업공간 설정할 것
+	out = heatmap.2(x,
+					col=topo.colors(100),
+					#ColSideColors=color.group,
+					scale = "row", # scale 기준방향 설정
+					trace="non",
+					density.info="none"
+					)
+	#dev.off()
+
+	return(t(out$carpet))
+}
+ 
+heatmap.fc = ssheatmap(data.fc)
+ 
+
+## Bibliography ##
+# http://www2.warwick.ac.uk/fac/sci/moac/people/students/peter_cock/r/heatmap/
+# ?heatmap; http://127.0.0.1:23779/library/stats/html/heatmap.html
+# ?heatmap.2; http://127.0.0.1:23779/library/gplots/html/heatmap.2.html
+
+write.table(heatmap,"heatmap.csv",sep=",",row.names=TRUE)
---
+++ clustering/ss.kmeans.r
@@ -0,0 +1,65 @@
+﻿### [R] Clustering.R ###
+# First written 2015-08-21 FRI
+# Last update 2016-05-02 MON
+# Version info 1.0a
+
+## Bibliography ##
+# http://manuals.bioinformatics.ucr.edu/home/R_BioCondManual#R_clustering
+
+## K-means Clustering ##
+
+##################################################
+# data =
+# ID	CD	CD	CD	CR85	CR85	CR85	CR70	CR70	CR70	CR55	CR55	CR55
+# 10413492	60.09154042	46.09345481	48.55203969	78.32549082	30.5870698	55.6334242	87.25706243	84.94929092	63.63462164	618.9117923	368.4589277	200.0532145
+# 10605666	76.37454632	39.64590628	55.2353513	20.51944777	19.68916467	32.4590754	3.990280042	3.755330806	11.47196005	3.563932468	5.039008885	3.592305385
+# 10537062	4654.788816	3379.513823	3827.535385	815.5728843	2723.601656	2901.533761	392.9195114	275.7429136	751.6201844	58.35158746	116.8885658	71.89053824
+# 10419568	146.9885533	121.0265701	136.6896313	16.03664006	48.78277761	35.32677143	18.50149569	25.46811008	14.80025369	18.43774098	15.73385186	19.84632529
+# 10467744	419.7190108	315.4570974	446.5630275	75.72774164	325.424712	409.2021741	45.49323114	49.07274265	60.52922608	41.11434671	29.76242139	42.24415982
+##################################################
+
+data = input(3)
+ 
+ss.kmeans = function(data,i) {
+	km = kmeans(t(scale(t(data))),i) 
+		# K-menas clustering
+		# row-wise scaling returns a mean close to zero and 
+		# a standard deviation of one.
+	#km$cluster
+	result.km = as.matrix(km$cluster)
+ 
+	# Designate color to each k-cluster #
+	mycol <- sample(rainbow(256))
+	mycol <- mycol[as.vector(km$cluster)]
+ 
+	# Hierarchical clustering #
+	data.mat = as.matrix(data)
+	hr = hclust(as.dist(1-cor(t(data.mat),method="pearson")),method="complete")
+	hc = hclust(as.dist(1-cor(data.mat, method="spearman")),method="complete")
+		# Correlation methods = "Pearson", "Kendall", "Spearman"
+		# hclust methods = "complete", "average", "median", "centroid", etc.
+	source("http://faculty.ucr.edu/~tgirke/Documents/R_BioCond/My_R_Scripts/my.colorFct.R")
+	#heatmap(data.mat, 
+	#        Rowv=as.dendrogram(hr), 
+	#        Colv=as.dendrogram(hc), 
+	#        col=my.colorFct(), 
+	#        scale="row")
+  
+	# Draw heatmap #
+	heatmap(data.mat, 
+			Rowv=as.dendrogram(hr), 
+			Colv=as.dendrogram(hc), 
+			col=my.colorFct(), 
+			scale="row", 
+			#ColSideColors=heat.colors(length(hc$labels)), 
+			RowSideColors=mycol) 
+		# This heatmap shows how the obtained K-means clusters can be 
+		# compared with the hierarchical clustering results by highlighting 
+		# them in the heatmap color bar.
+  
+	return(result.km)
+}
+ 
+result.km = ss.kmeans(data,2)
+ 
+write.table(result.km,"kmeans.csv",sep=",",row.names=TRUE)
