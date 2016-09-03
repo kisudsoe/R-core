b2e4cbadb9c8c0b8edc2e3835e30209e16a7e2c6
kisudsoe <kisudsoe@gmail.com>
2016-09-03 13:12:22 +0900

    ss.gene.selec 함수_v1.1 사소한수정

--- data manipulation/ss.gene.selec.r
+++ data manipulation/ss.gene.selec.r
@@ -2,9 +2,9 @@
 ## Ver 1.0	- 150903, original version of this function
 ## ver 1.1	- 160429, using for Yeast HD LD Rho project
 
-ss.gene.selec = function(data.fc,fc=1.5,data.fdr,fcfdr) {
+ss.gene.selec = function(data.fc,fc=1.5,data.fdr,row.names) {
 	n= length(colnames(data.fc))
-  
+
 	for(i in 1:n) {
 		fdr1 = which(data.fdr<=0.01 & abs(data.fc[,i])>fc)
 		f1 = length(fdr1)
@@ -13,7 +13,7 @@ ss.gene.selec = function(data.fc,fc=1.5,data.fdr,fcfdr) {
 		fdr10 = which(data.fdr<=0.1 & abs(data.fc[,i])>fc)
 		f10 = length(fdr10)
 		num = c(f1,f5,f10)
-    
+
 		if(i==1) {
 			result = num
 			fdr1.id = fdr1
@@ -26,19 +26,19 @@ ss.gene.selec = function(data.fc,fc=1.5,data.fdr,fcfdr) {
 			fdr10.id = union(fdr10.id,fdr10)
 		}
 	}
-  
+
 	num = c(length(fdr1.id),length(fdr5.id),length(fdr10.id))
 	result = cbind(result,num)
 	colnames(result) = c(colnames(data.fc),"Union")
 	#rownames(result) = c("fc2&fdr1","fc2&fdr5","fc2&fdr10")
-	rownames(result) = fcfdr
-	print(fcfdr)
+	rownames(result) = row.names
+	print(row.names)
 
 	result.summ = list(result=result,
                        fdr1.id=fdr1.id,
                        fdr5.id=fdr5.id,
                        fdr10.id=fdr10.id)
-  
+
 	return(result.summ)
 }
 
