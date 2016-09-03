aa4d73f917d0d205b287cf39417d654888571a4f
kisudsoe <kisudsoe@gmail.com>
2016-09-03 13:11:11 +0900

    ss.anova.fdr2 함수_v2.1a

--- statistics/ss.anova.fdr2.r
+++ statistics/ss.anova.fdr2.r
@@ -1,6 +1,7 @@
 # 2016-05-06 ver
 ## ver 2.0	- 160503, Advanced process speed than 'ss.anova.fdr' function
 ## ver 2.1	- 160506, set rownames to result for Yeast HDLDRho project
+## ver 2.1a - 160811, eliminate qvalue function (unavailable package)
 
 ss.anova.fdr2 = function(data,group) {
 	library(pbapply) # package for progressbar, install needed
@@ -8,18 +9,22 @@ ss.anova.fdr2 = function(data,group) {
 	out = NULL
 	raw.p = NULL
 	time1 = Sys.time()
-  
+
 	# Step 1. ANOVA iteration
-	raw.p = pbapply(data,1,function(row){
-				  anovaP = anova(lm(row~group))$'Pr(>F)'[1]})
+	raw.p = pbapply(data,1,function(row) {
+								if(!any(is.na(row))) {
+									fit = lm(row~group,na.action=na.exclude)
+									anovaP = anova(fit)$'Pr(>F)'[1]
+								} else { anovaP = NA } })
 	time2 = Sys.time()
 	cat('Process iteration =',n,'\n')
 	print(time2-time1)
 	cat('----------------------------------\n\n')
-  
+
 	# Step 2. Adjusted p-values
 	raw.p = as.vector(raw.p)
-  
+	length(raw.p)
+
 	library(fdrtool)
 	#pval.estimate.eta0(p1,method="bootstrap") # Draw plot
 	q = fdrtool(raw.p,statistic="pvalue")
@@ -27,12 +32,12 @@ ss.anova.fdr2 = function(data,group) {
 	cat('(1/9) fdrtool.tfdr\t-> done\n')
 	fdrtool.lfdr = q$lfdr
 	cat('(2/9) fdrtool.dfdr\t-> done\n')
-  
-	library(qvalue)
-	q2 = qvalue(raw.p)
-	qvalue.fdr = q2$qvalues
-	cat('(3/9) qvalue.fdr\t-> done\n')
-  
+
+	#library(qvalue)
+	#q2 = qvalue(raw.p)
+	#qvalue.fdr = q2$qvalues
+	cat('(3/9) qvalue.fdr\t-> skip, package is not available (for R version 3.3.1)\n')
+
 	stat.BH.fdr = p.adjust(raw.p,"BH")
 	cat('(4/9) stat.fdr.BH\t-> done\n')
 	#stat.Hommel = p.adjust(raw.p,"hommel") # take time too long
@@ -45,22 +50,22 @@ ss.anova.fdr2 = function(data,group) {
 	cat('(8/9) stat.Holm\t\t-> done\n')
 	stat.Bonferroni = p.adjust(raw.p,"bonferroni")
 	cat('(9/9) stat.Bonferroni\t-> done\n')
-  
+
 	result = data.frame(raw.p,
-                        fdrtool.qval,
-                        fdrtool.lfdr,
-                        qvalue.fdr,
-                        stat.BH.fdr,
-                        #stat.Hommel,
-                        stat.BY,
-                        stat.Hochberg,
-                        stat.Holm,
-                        stat.Bonferroni)
+                      fdrtool.qval,
+                      fdrtool.lfdr,
+                      #qvalue.fdr,
+                      stat.BH.fdr,
+                      #stat.Hommel,
+                      stat.BY,
+                      stat.Hochberg,
+                      stat.Holm,
+                      stat.Bonferroni)
 	rownames(result) = rownames(data) # set row names
-  
+
 	## Plot draw start ##
 	result.r = result[order(raw.p),] # re-ordered by raw.p
-  
+
 	matplot(result.r$raw.p, result.r, xlim=c(0,1),ylim=c(0,1),
             main="Various multiple test correction methods",
             xlab="Raw p-value",
@@ -72,8 +77,8 @@ ss.anova.fdr2 = function(data,group) {
 	legend('bottomright',
            legend=c("raw.p",
                     "fdrtool.tfdr",
-					"fdrtool.dlfdr",
-                    "qvalue.fdr",
+										"fdrtool.dlfdr",
+                    #"qvalue.fdr",
                     "stat.BH.fdr",
                     #"stat.Hommel",
                     "stat.BY",
@@ -83,25 +88,25 @@ ss.anova.fdr2 = function(data,group) {
 		   col=c("black",rainbow(9)), # Set color
            cex=1,
            pch=16)
-  
+
 	x11()
 	par(mfrow=c(2,2)) # 2x2 plot partition
 	hist(raw.p, xlim=c(0,1))
-	hist(fdrtool.qval, xlim=c(0,1))
 	hist(stat.BH.fdr, xlim=c(0,1))
+	hist(stat.Holm, xlim=c(0,1))
 	hist(stat.Bonferroni, xlim=c(0,1))
-  
+
 	cat('\nPlot draw\t\t-> done\n')
 	cat('----------------------------------\n')
 	## plot draw done ##
-  
+
 	#######################
 	# Duration time check #
 	#######################
 	time2 = Sys.time()
 	print(time2-time1)
 	#######################
-  
+
 	return(result)
 }
 
