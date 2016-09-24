15038386f2c36534fd62cbf803bf555e9ef2e970
kisudsoe <kisudsoe@gmail.com>
2016-09-24 17:21:21 +0900

    corr_pval 함수 (cmd 실행) v1_사소한 수정

--- statistics/corr_pval.cmd
+++ statistics/corr_pval.cmd
@@ -12,7 +12,7 @@ set csvcount=0
 if exist *.csv (
   : make 'result' folder
   if not exist result md result
-  
+
   for %%a in (*.csv) do (
     set /a csvcount=!scvcount!+1
     rscript corr_pval.cmd.r "%%a" "result\%%a"
@@ -39,5 +39,6 @@ exit
 :: 참고 ::
 : 현재 실행중인 배치파일의 경로 얻기 https://sunhyeon.wordpress.com/2012/08/05/161/
 : 배치파일 명령어 모음 http://snoopybox.co.kr/1404
+: 배치파일 명령어 모음 http://mssp.tistory.com/entry/배치파일-명령어
 : 배치파일 언어 규칙 http://jangpd007.tistory.com/163
 : r 스크립트 cmd 실행 http://bhpark.tistory.com/61
