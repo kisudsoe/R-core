33404595d941f85b252f4601f2c2e61421839c23
kisudsoe <kisudsoe@gmail.com>
2016-10-03 11:01:47 +0900

    ss.pca 함수_160930 코드 추가

--- data classification/ss.pca.r
+++ data classification/ss.pca.r
@@ -1,3 +1,52 @@
+# 2016-09-30 FRI
+## PCA for total data
+pca_total = PCA(t(yeast.cerev.sgl[,2:61])) # pca by arrays
+pca_total_coord = pca_total$ind$coord %>% as.data.frame
+pca_total_coord = cbind(pca_total_coord,
+                        groups=group.info$groups[2:61],
+                        groups2=group.info$groups2[2:61])
+a_hull = function(pca_total_coord) {
+  pca_total_coord[chull(pca_total_coord$Dim.1,
+                          pca_total_coord$Dim.2),]
+}
+pca_total_hull = ddply(pca_total_coord,"groups2",a_hull)
+rm(a_hull)
+
+## PCA for 669 DEGs
+pca_669degs = PCA(t(fc2bon5_669degs[,2:61])) # pca by arrays
+pca_669degs_coord = pca_669degs$ind$coord %>% as.data.frame
+pca_669degs_coord = cbind(pca_669degs_coord,
+                          groups=group.info$groups[2:61],
+                          groups2=group.info$groups2[2:61])
+a_hull = function(pca_669degs_coord) {
+  pca_669degs_coord[chull(pca_669degs_coord$Dim.1,
+                          pca_669degs_coord$Dim.2),]
+}
+pca_669degs_hull = ddply(pca_669degs_coord,"groups2",a_hull)
+pca_669degs_hull
+rm(a_hull)
+
+## PCA plot
+par(xpd=F)
+ggplot()+theme_bw()+ # plot for total
+  geom_point(data=pca_total_coord,
+       aes(Dim.1,Dim.2,colour=groups,pch=groups2),size=3)+
+  geom_polygon(data=pca_total_hull,
+               aes(Dim.1,Dim.2,fill=groups2),alpha=0.3)+
+  labs(title="PCA reulst of total",
+       x=paste0("Dim 1 (",round(pca_total$eig[1,2],2),"%)"),
+       y=paste0("Dim 2 (",round(pca_total$eig[2,2],2),"%)"))
+
+ggplot()+theme_bw()+ # plot for 669 degs
+  geom_point(data=pca_669degs_coord,
+       aes(Dim.1,Dim.2,colour=groups,pch=groups2),size=3)+
+  geom_polygon(data=pca_669degs_hull,
+               aes(Dim.1,Dim.2,fill=groups2),alpha=0.3)+
+  labs(title="PCA reulst of 669 DEGs",
+       x=paste0("Dim 1 (",round(pca_total$eig[1,2],2),"%)"),
+       y=paste0("Dim 2 (",round(pca_total$eig[2,2],2),"%)"))
+
+
 # 2016-06-29 WED, 06-30 THU----
 ## R update to v3.3.1
 install.packages("installr")
@@ -24,11 +73,13 @@ fa1 = factanal(data_res, factor=3, rotation="varimax") # Should be no 'NA' value
 fa1
 
 
-# 2016-07-01 FRI----
-## PCAplot function for displaying PCA result
+# PCAplot function for displaying PCA result
 ## chull function for draw borderline of dots in scatter plot
 ## ver 1.0	- 160701, Dog project
-PCAplot = function(PCAout,criteria,range=NULL,
+## ver 1.0a - 160930, minor fix
+library(FactoMineR)
+PCAout = PCA(data)
+ss.pcaplot = function(PCAout,criteria,range=NULL,
                    crite2=NULL,range2=NULL,hulls=TRUE,labels=TRUE) {
   #par(mar=c(5,5,5,5), xpd=TRUE) # (Bottom, Left, Top, Right)
   par(xpd=FALSE)
@@ -38,7 +89,7 @@ PCAplot = function(PCAout,criteria,range=NULL,
        main="Individuals factor map (PCA)")
   abline(h=0,lty=2)
   abline(v=0,lty=2)
-  
+
   criteria.title = names(criteria)[1]
   if(is.null(range)){
     criteria = as.factor(criteria)
@@ -48,7 +99,7 @@ PCAplot = function(PCAout,criteria,range=NULL,
   }
   cols = rainbow(length(criteria.lv))
   colstrans = rainbow(length(criteria.lv), alpha=0.3)
-  
+
   ## This section for crite2 border hull polygons
   if(!is.null(crite2)){
     crite2.title = names(crite2)[1]
@@ -58,7 +109,7 @@ PCAplot = function(PCAout,criteria,range=NULL,
       crite2.lv = range2
     cols2 = gray.colors(length(crite2.lv))
     colstrans2 = gray.colors(length(crite2.lv),alpha=0.3)
-    
+
     for(i in 1:length(crite2.lv)){
       # getting the convex hull of each unique point set
       if(is.null(range2)){
@@ -77,7 +128,7 @@ PCAplot = function(PCAout,criteria,range=NULL,
     }
     legend("topright",title=crite2.title,legend=crite2.lv,pch=15,col=cols2)
   }
-  
+
   for(i in 1:length(criteria.lv)){
     if(is.null(range)) {
       criteriaIds = which(criteria==criteria.lv[i])
@@ -90,14 +141,14 @@ PCAplot = function(PCAout,criteria,range=NULL,
     }
     Coord = PCAout$ind$coord[criteriaIds,1:2]
     points(Coord, pch=19, col=cols[i])
-    
+
     # getting the convex hull of each unique point set
     if(hulls==TRUE) {
       hullcoor = Coord[chull(Coord[,1],Coord[,2]),]
       polygon(hullcoor,col=colstrans[i], border=colstrans[i]) # border=cols[i]
     }
   }
-  
+
   #par(xpd=TRUE)
   legend("topleft",title=criteria.title,legend=criteria.lv,pch=19,col=cols)
   if(labels==TRUE) {
@@ -107,13 +158,13 @@ PCAplot = function(PCAout,criteria,range=NULL,
 
 category = setNames(res1$call$X$Age,"Age")
 category2 = setNames(res1$call$X$Species,"Species")
-PCAplot(PCAout=res1,criteria=category,
-        #range=c(0.65,0.7,0.75,0.85), # AT ratio
-        #range=c(20,25,30,35,45), # Weight
-        range=c(4,8,14), # Age
-        crite2=category2,
-        #range2=c(0.65,0.7,0.75,0.85), # AT ratio
-        hulls=FALSE,labels=FALSE)
+ss.pcaplot(PCAout=res1,criteria=category,
+           #range=c(0.65,0.7,0.75,0.85), # AT ratio
+           #range=c(20,25,30,35,45), # Weight
+           range=c(4,8,14), # Age
+           crite2=category2,
+           #range2=c(0.65,0.7,0.75,0.85), # AT ratio
+           hulls=FALSE,labels=FALSE)
 
 summary(res1$call$X)
 summary(as.factor(category))
