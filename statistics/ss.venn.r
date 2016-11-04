dc5a2b7c50eabada4a5c3c41339c53cacf116b42
kisudsoe <kisudsoe@gmail.com>
2016-11-04 20:47:44 +0900

    ss.venn 함수_사소한 수정

--- statistics/ss.venn.r
+++ statistics/ss.venn.r
@@ -1,7 +1,7 @@
 ﻿# 2016-05-04 WED
 ## ver 1.0	- 160504, First version using for Human X-ALD project, Yeast project
 ## Ver 1.0a - 160505, Add function for print process information out
-ss.tukey3Venn = function(tukey,group) { # 160504 row[2] is centered.
+ss.tukey3Venn = function(tukey) { # 160504 row[2] is centered.
 	library(pbapply)
 	time1 = Sys.time()
 	# Make T/F table
@@ -24,11 +24,11 @@ ss.tukey3Venn = function(tukey,group) { # 160504 row[2] is centered.
 					  paste(coln[2],"!=",coln[3],sep=""),
 					  paste(coln[1],"!=",coln[3],sep=""))
 	print(out[1:10,])
-	
+
 	# Make Venn Diagram
 	library(limma)
 	vennDiagram(out) # Generate Venn Diagram
-	
+
 	# Make return table
 	out = data.frame(tukey,out)
 	time2 = Sys.time()
@@ -36,7 +36,7 @@ ss.tukey3Venn = function(tukey,group) { # 160504 row[2] is centered.
 	return(out)
 }
 
-sgnl.cerev_tukey_venn = ss.tukey3Venn(sgnl.cerev_tukey,group)
+sgnl.cerev_tukey_venn = ss.tukey3Venn(sgnl.cerev_tukey)
 
 
 # Venn Diagram (3 circles)
@@ -48,7 +48,7 @@ biocLite("limma")
 ## v1.0 2015 - original version
 ## v1.1 160704 - Add names of venn from vector
 ss.venn3 = function(group1, group2, group3, main="") { # 160704 ver - id for each 3-groups
-    
+
 	# Generate union group from input three groups
 	unionlist = union(group1,group2)
 	unionlist = union(unionlist,group3)
@@ -61,29 +61,29 @@ ss.venn3 = function(group1, group2, group3, main="") { # 160704 ver - id for eac
 						 g1=character(length(unionlist)),
 						 g2=character(length(unionlist)),
 						 g3=character(length(unionlist)))
-  
+
 	unionPr$g1 = group1[match(unionPr$list,group1)]
 	unionPr$g2 = group2[match(unionPr$list,group2)]
 	unionPr$g3 = group3[match(unionPr$list,group3)]
-    
+
 	rownames(unionPr) = unionPr[,1] 	# column 1 list as rownames
 	print(head(rownames(unionPr)))
 	unionPr[1] = NULL 					# delete column 1
-  
+
 	# Make TRUE/FALSE table to match with ID list
 	union = (unionPr != "") 			# Transform values to TRUE, if ID exits
 	union[is.na(union)] = FALSE			# Transform NA to FALSE value
-  
+
 	union = as.data.frame(union) 		# Make 'union' to data.frame form
 	title = c(paste(title[1],'\n',length(group1)),
 			  paste(title[2],'\n',length(group2)),
 			  paste(title[3],'\n',length(group3)))
 	colnames(union) = title 			# Names attach to venn diagram
-  
+
 	library(limma)
 	union = list(list=union, vennCounts=vennCounts(union))
 	vennDiagram(union$list,main=main) 			# Generate Venn Diagram
-  
+
 	return(union)
 }
 
@@ -111,18 +111,18 @@ ss.venn2 = function(group1,group2) { #Venn for 2-groups
 	union = (unionPr != "") # TRUE, if id exist.
 	union[is.na(union)] = FALSE # FALSE, if id null.
 	print(head(union))
-  
+
 	library(limma)
 	colnames(union) = c(length(group1),length(group2))
 	union = list(list=union, vennCounts=vennCounts(union))
 	vennDiagram(union$list)
-  
+
 	return(union)
 }
 
 sgd.Affy_venn = ss.venn2(sgd.id$gene.primaryIdentifier,
                          sgd.Affyid$SGD.accession.number)
-						 
+
 # 2016-05-16 MON
 ## ver 1.0 - 160516, Coded for general
 ss.venn4 = function(group1,group2,group3,group4) {
@@ -141,11 +141,11 @@ ss.venn4 = function(group1,group2,group3,group4) {
 	union = (unionPr != "") # TRUE, if id exist.
 	union[is.na(union)] = FALSE # FALSE, if id null.
 	print(head(union))
-  
+
 	library(limma)
 	colnames(union) = c(length(group1),length(group2),length(group3),length(group4))
 	union = list(list=union, vennCounts=vennCounts(union))
 	vennDiagram(union$list)
-  
+
 	return(union)
 }
