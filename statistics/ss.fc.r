88888d1fc362a8a751b50629be2a2869599ce659
kisudsoe <kisudsoe@gmail.com>
2017-03-23 23:02:57 +0900

    ss.fc 함수_v1.1 계산 효율 증대

--- statistics/ss.fc.r
+++ statistics/ss.fc.r
@@ -1,9 +1,10 @@
 # 2016-05-01 SUN
 ## ver 1.0	- 160501, Yeast HD LD Rho project
 ## ver 1.0a	- 160504, Bugfix: as.factor(group)
+## ver 1.1  - 161114, add option: could set base group
 
-ss.fc = function(dat,group) {
-	library(pbapply) # package for progressbar, install needed
+ss.fc = function(data,group,base=NULL) {
+	library(pbapply) # package for progressbar
 	group.lv = levels(as.factor(group)) # bugfix 160504
 	n = length(group.lv)
 	cat('Level(s) of group =',group.lv,'\n\n')
@@ -11,32 +12,38 @@ ss.fc = function(dat,group) {
 
 	# Step 1. Calculate average of each group
 	for(i in 1:n) {
-		av = pbapply(dat[,which(group==group.lv[i])],1,mean) # apply: 1-rows, 2-columns
+		av = pbapply(data[,which(group==group.lv[i])],1,mean)
 		avs = cbind(avs,av)
 	}
 	colnames(avs) = group.lv # set colnames of avs df
 	print(head(avs))
 
 	# Step 2. Calculate fold-changes by every combination
-	comb = combn(n,2) # compute combination
+	if(length(base)==0) comb = combn(n,2) # compute combination
+	else if(length(base)>0) { # add code 161114
+	  n1 = which(group.lv==base)
+	  comb = t(matrix(c(setdiff(c(1:n),n1),rep(n1,n-1)),ncol=2))
+	  print(comb)
+	}
 	m = length(comb[1,])
+	cat('Process iteration = ',m,'\n')
 	fcs = NULL
 	fc.col = NULL
 	for(i in 1:m) {
 		fc.col = c(fc.col,paste(group.lv[comb[1,i]],'/',
 								group.lv[comb[2,i]],sep=""))
-		cat('Process =',i,'/',m,'\n')
+		#cat('Process =',i,'/',m,'\n')
 		fc = avs[,comb[1,i]]-avs[,comb[2,i]]
 		fcs = cbind(fcs, fc)
 	}
 	colnames(fcs) = fc.col
-	print(head(fcs))
-	fcs2 = apply(fcs,2,function(x){ifelse(x<0,-2^(-x),2^x)}) # apply: 1-rows, 2-columns
-	print(head(fcs2))
+	print(tbl_df(fcs))
+	fcs2 = pbapply(fcs,2,function(x){ifelse(x<0,-2^(-x),2^x)})
+	print(tbl_df(fcs2))
 
 	# Step 3. Collect results
-	#out = as.data.frame(cbind(avs,fcs,fcs2))
 	return(as.data.frame(fcs2))
 }
-
-fc.cerev = as.data.frame(ss.fc(sgnl.cerev,group))
+cerev.fc = ss.fc(data=cerev.sgl[,-1],
+                 group=droplevels(group.info$groups[-1]),
+                 base="NR_005h")
